efc98aacad34d83011f36dbef7b2aeb1bc80fc50
ADAM David Alan Martin <adam.martiN@10gen.com>
2016-09-21 07:54:48 -0400

    Some documentation for AutoRAII.

--- src/mongo/base/ming/autoraii.h
+++ src/mongo/base/ming/autoraii.h
@@ -12,13 +12,17 @@ namespace mongo
 {
 	namespace ming
 	{
-		namespace detail { class Na; }
+		namespace detail { class Na; } // The detail::Na class is used to indicate an absent parameter to AutoRAII
 		template< typename T= detail::Na > class AutoRAII;
 
+        // This form is intended for use with things like socket_t, unix file handles, and other primitive types.
 		template< typename T >
 		class AutoRAII
 		{
 			private:
+                static_assert( !std::is_class< T >::value,
+                        "AutoRAII cannot adapt to structs or classes.  Consider adding a dtor to your type or using a pointer to the struct." );
+
 				std::function< void ( T ) > dtor;
 				T resource;
 				AutoRAII( const AutoRAII & )= delete;
@@ -34,12 +38,10 @@ namespace mongo
 					this->dtor( this->resource );
 				}
 
-				operator const T &() const
-				{
-					return this->resource;
-				}
+				operator const T &() const { return this->resource; }
 		};
 
+        // This form is intended for use with any pointer that has cleanup semantic functions.
 		template< typename T >
 		class AutoRAII< T * >
 		{

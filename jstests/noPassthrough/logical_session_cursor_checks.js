8a471bc97fb0a5af357f7d71ff2aa2bcc68d2df9
ADAM David Alan Martin <adam.martin@10gen.com>
2017-08-28 14:55:56 -0400

    Move authorization check implementation to CPP.
    Also rewrite jstest for testing in Ubuntu.

--- jstests/noPassthrough/logical_session_cursor_checks.js
+++ jstests/noPassthrough/logical_session_cursor_checks.js
@@ -1,12 +1,9 @@
 (function() {
     'use strict';
 
-    var conn = MongoRunner.runMongod({nojournal: ""});
+    var conn = MongoRunner.runMongod({auth: "", nojournal: ""});
     var admin = conn.getDB("admin");
-
-    conn = MongoRunner.runMongod({auth: "", nojournal: ""});
-    admin = conn.getDB("admin");
-    data = conn.getDB("data_storage");
+    var data = conn.getDB("data_storage");
 
     admin.createUser({user: 'admin', pwd: 'admin', roles: jsTest.adminUserRoles});
     admin.auth("admin", "admin");
@@ -15,19 +12,22 @@
     data.createUser({user: 'user1', pwd: 'password', roles: jsTest.basicUserRoles});
     admin.logout();
 
+
     data.auth("user0", "password");
-    assert.writeOK(data.insert({name: "first", data: 1}));
-    assert.writeOK(data.insert({name: "second", data: 2}));
+    assert.writeOK(data.test.insert({name: "first", data: 1}));
+    assert.writeOK(data.test.insert({name: "second", data: 2}));
 
-    var res = assert.commandWorked(data.runCommand({startSession: 1}));
-	var sessionId = res.id;
-    res = assert.commandWorked(data.runCommand({find: "data_storage", batchSize: 0}));
-    var cursorId = res.cursor.id;
-    assert.commandWorked(testDB.runCommand({getMore: cursorId, collection: "data_storage"}));
+    {
+		var session1 = conn.startSession();
+		var session2 = conn.startSession();
+        res = assert.commandWorked(session1.getDatabase("data_storage").runCommand({find: "test", batchSize: 0}));
+        var cursorId = res.cursor.id;
+        assert.commandWorked(session1.getDatabase("data_storage").runCommand({getMore: cursorId, collection: "test"}));
+        assert.commandFailed(session2.getDatabase("data_storage").runCommand({getMore: cursorId, collection: "test"}));
 
-    assert.commandWorked(data.runCommand({startSession: 2}));
-    assert.commandFailedWithCode(data2.runCommand({getMore: cursorId, collection: "data_storage"}),
-                                 ErrorCodes.Unauthorized);
+		session2.endSession();
+		session1.endSession();
+    }
 
     MongoRunner.stopMongod(conn);
 })();

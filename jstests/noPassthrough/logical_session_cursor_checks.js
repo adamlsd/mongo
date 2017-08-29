b21e1256be898907c271950f7c22c11d89d2a8d9
ADAM David Alan Martin <adam.martin@10gen.com>
2017-08-29 14:40:48 -0400

    Comment specific test parts, and fix lint.

--- jstests/noPassthrough/logical_session_cursor_checks.js
+++ jstests/noPassthrough/logical_session_cursor_checks.js
@@ -12,28 +12,33 @@
     data.createUser({user: 'user1', pwd: 'password', roles: jsTest.basicUserRoles});
     admin.logout();
 
-
     data.auth("user0", "password");
     assert.writeOK(data.test.insert({name: "first", data: 1}));
     assert.writeOK(data.test.insert({name: "second", data: 2}));
 
+    // Test that getMore works correctly on the same session.
     {
         var session1 = conn.startSession();
         var session2 = conn.startSession();
-        var res = assert.commandWorked(session1.getDatabase("data_storage").runCommand({find: "test", batchSize: 0}));
+        var res = assert.commandWorked(
+            session1.getDatabase("data_storage").runCommand({find: "test", batchSize: 0}));
         var cursorId = res.cursor.id;
-        assert.commandWorked(session1.getDatabase("data_storage").runCommand({getMore: cursorId, collection: "test"}));
+        assert.commandWorked(session1.getDatabase("data_storage")
+                                 .runCommand({getMore: cursorId, collection: "test"}));
 
         session2.endSession();
         session1.endSession();
     }
 
+    // Test that getMore correctly gives an error, when using a cursor on a different session.
     {
         var session1 = conn.startSession();
         var session2 = conn.startSession();
-        var res = assert.commandWorked(session1.getDatabase("data_storage").runCommand({find: "test", batchSize: 0}));
+        var res = assert.commandWorked(
+            session1.getDatabase("data_storage").runCommand({find: "test", batchSize: 0}));
         var cursorId = res.cursor.id;
-        assert.commandFailed(session2.getDatabase("data_storage").runCommand({getMore: cursorId, collection: "test"}));
+        assert.commandFailed(session2.getDatabase("data_storage")
+                                 .runCommand({getMore: cursorId, collection: "test"}));
 
         session2.endSession();
         session1.endSession();

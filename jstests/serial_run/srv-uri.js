5829ae642b49b4eea36ba73fcb90f92c651e385f
ADAM David Alan Martin <adam.martin@mongodb.com>
2017-11-29 10:51:10 -0500

    SERVER-32111 Make `srv-uri` test not use SSL
    The `srv-uri` test needs to explicitly decline to use SSL, since
    the new `mongo+srv://` URI scheme implicitly turns on SSL.  On
    SSL based configurations, the test will fail to connect to
    localhost when SSL is enabled.

--- jstests/serial_run/srv-uri.js
+++ jstests/serial_run/srv-uri.js
@@ -2,8 +2,9 @@
     "use strict";
     const md = MongoRunner.runMongod({port: "27017", dbpath: MongoRunner.dataPath});
     assert.neq(null, md, "unable to start mongod");
+    const targetURI = 'mongodb+srv://test1.test.build.10gen.cc./?ssl=false';
     const exitCode =
-        runMongoProgram('mongo', 'mongodb+srv://test1.test.build.10gen.cc.', '--eval', ';');
+        runMongoProgram('mongo', targetURI, '--eval', ';');
     assert.eq(exitCode, 0, "Failed to connect with a `mongodb+srv://` style URI.");
     MongoRunner.stopMongod(md);
 })();

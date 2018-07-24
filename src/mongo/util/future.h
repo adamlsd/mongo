3e426dd74088a247ecc4846cbf1436f250847752
ADAM David Alan Martin <adam.martin@10gen.com>
2018-07-24 16:52:16 -0400

    Crazy `friend`ship.

--- src/mongo/util/future.h
+++ src/mongo/util/future.h
@@ -480,8 +480,7 @@ struct SharedStateImpl final : SharedStateBase {
 using future_details::Promise;
 using future_details::Future;
 
-template <typename T>
-auto makePromiseFuture();
+class PromisePermission;
 
 /**
  * This class represents the producer side of a Future.
@@ -578,15 +577,16 @@ public:
      */
     SharedPromise<T> share() noexcept;
 
+
+    // This is not public because we found it frequently was involved in races.  The
+    // `makePromiseFuture<T>` API avoids those races entirely.
+    Future<T> getFuture(PromisePermission) noexcept;
+
 private:
     template <typename>
     friend auto makePromiseFuture();
 
     friend class Future<void>;
-
-    // This is not public because we found it frequently was involved in races.  The
-    // `makePromiseFuture<T>` API avoids those races entirely.
-    Future<T> getFuture() noexcept;
 
 
     template <typename Func>
@@ -1309,6 +1309,19 @@ auto makeReadyFutureWith(Func&& func) {
     return Future<void>::makeReady().then(std::forward<Func>(func));
 }
 
+PromisePermission makePromisePermission();
+
+class EvilBase {
+protected:
+    EvilBase() = default;
+};
+class PromisePermission : EvilBase {
+private:
+    PromisePermission() = default;
+
+    friend PromisePermission makePromisePermission();
+};
+
 /**
  * Returns a bound Promise and Future in a struct with friendly names (promise and future) that also
  * works well with C++17 structured bindings.
@@ -1317,9 +1330,13 @@ template <typename T>
 inline auto makePromiseFuture() {
     struct PromiseAndFuture {
         Promise<T> promise;
-        Future<T> future = promise.getFuture();
+        Future<T> future = promise.getFuture(makePromisePermission());
     };
     return PromiseAndFuture();
+}
+
+inline PromisePermission makePromisePermission() {
+    return PromisePermission{};
 }
 
 /**
@@ -1353,7 +1370,7 @@ using FutureContinuationResult =
 //
 
 template <typename T>
-inline Future<T> Promise<T>::getFuture() noexcept {
+inline Future<T> Promise<T>::getFuture(PromisePermission) noexcept {
     _sharedState->threadUnsafeIncRefCountTo(2);
     return Future<T>(boost::intrusive_ptr<SharedState<T>>(_sharedState.get(), /*add ref*/ false));
 }

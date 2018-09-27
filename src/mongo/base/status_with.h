4ce2b4165bc34e840ae0f0ba16ed8ca4c922936c
ADAM David Alan Martin <adam.martin@10gen.com>
2018-09-27 15:36:57 -0400

    SERVER-37360 `StatusWith` implicit conversions support.
    This uses a variation of the technique used in `std::optional` to
    permit chained implicit conversions in constructors.

--- src/mongo/base/status_with.h
+++ src/mongo/base/status_with.h
@@ -28,6 +28,7 @@
 #pragma once
 
 #include <boost/optional.hpp>
+#include <cstddef>
 #include <iosfwd>
 #include <type_traits>
 #include <utility>
@@ -84,8 +85,21 @@ using StatusOrStatusWith = std::conditional_t<std::is_void<T>::value, Status, St
  */
 template <typename T>
 class MONGO_WARN_UNUSED_RESULT_CLASS StatusWith {
+private:
     MONGO_STATIC_ASSERT_MSG(!isStatusOrStatusWith<T>,
                             "StatusWith<Status> and StatusWith<StatusWith<T>> are banned.");
+    // `TagTypeBase` is used as a base for the `TagType` type, to prevent it from being an
+    // aggregate.
+    struct TagTypeBase {
+    protected:
+        TagTypeBase() = default;
+    };
+    // `TagType` is used as a placeholder type in parameter lists for `enable_if` clauses.  They
+    // have to be real parameters, not template parameters, due to MSVC limitations.
+    class TagType : TagTypeBase {
+        TagType() = default;
+        friend StatusWith;
+    };
 
 public:
     using value_type = T;
@@ -114,6 +128,12 @@ public:
      * for the OK case
      */
     StatusWith(T t) : _status(Status::OK()), _t(std::move(t)) {}
+
+    template <typename Alien>
+    StatusWith(Alien&& alien,
+               typename std::enable_if_t<std::is_convertible<Alien, T>::value, TagType> = makeTag(),
+               typename std::enable_if_t<!std::is_same<Alien, T>::value, TagType> = makeTag())
+        : StatusWith(static_cast<T>(std::forward<Alien>(alien))) {}
 
     const T& getValue() const {
         dassert(isOK());
@@ -149,6 +169,12 @@ public:
     void status_with_transitional_ignore() const& noexcept = delete;
 
 private:
+    // The `TagType` type cannot be constructed as a default function-parameter in Clang.  So we use
+    // a static member function that initializes that default parameter.
+    static TagType makeTag() {
+        return {};
+    }
+
     Status _status;
     boost::optional<T> _t;
 };

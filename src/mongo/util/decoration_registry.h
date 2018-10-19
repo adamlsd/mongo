7e51c33c4002dbbac4f2fc5cb420b27f5cc5741a
ADAM David Alan Martin <adam.martin@10gen.com>
2018-10-19 13:59:41 -0400

    SERVER-35143 Destroy decorations in reverse order.
    To better match what normal behavior is for C++ destructors of
    objects, the Decoration system should destroy decorations in
    the reverse order of their creation.

--- src/mongo/util/decoration_registry.h
+++ src/mongo/util/decoration_registry.h
@@ -115,9 +115,11 @@ public:
      * Called by the DecorationContainer destructor.  Do not call directly.
      */
     void destroy(DecorationContainer<DecoratedType>* const container) const noexcept try {
-        for (auto& decoration : _decorationInfo) {
-            decoration.destructor(container->getDecoration(decoration.descriptor));
-        }
+		std::for_each(_decorationInfo.rbegin(), _decorationInfo.rend(),
+			[&container]( const auto &decoration )
+			{
+				decoration.destructor(container->getDecoration(decoration.descriptor));
+			});
     } catch (...) {
         std::terminate();
     }

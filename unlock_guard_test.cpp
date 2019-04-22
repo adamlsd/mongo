91bb1d4bb30c99e030df38c3349e206a4d584c99
ADAM David Alan Martin <adam.martin@10gen.com>
2019-04-22 17:01:51 -0400

    Partially working.

--- unlock_guard_test.cpp
+++ unlock_guard_test.cpp
@@ -16,12 +16,35 @@ namespace InfiniteMonkeys
 		struct Evil {};
     //}
 
+	class Poisonable : boost::noncopyable
+	{
+		private:
+			struct C { enum State { Healthy, Poisoned }; };
+			using State= C::State;
+
+			State state= C::Healthy;
+			Poisonable *const parent= nullptr;
+
+		protected:
+			[[nodiscard]] bool alive() const { return this->state != C::Poisoned; }
+			void validate() const { assert( alive() ); }
+
+			void revive() & { this->state= C::Healthy; }
+			void poison() & { this->state= C::Poisoned; }
+
+			explicit Poisonable( Poisonable *const i_parent ) : parent( i_parent ) { if( this->parent ) this->parent->poison(); }
+
+		public:
+			~Poisonable() { this->validate(); if( this->parent ) this->parent->revive(); }
+	};
+
 	class OwningUnlockable;
 
-    class Unlockable : boost::noncopyable
+    class Unlockable : Poisonable
     {
         private:
             ULock *lk;
+			struct Poisoner;
 			Poisoner *poisoner= nullptr;
 
 			struct Poisoner
@@ -32,12 +55,11 @@ namespace InfiniteMonkeys
 				~Poisoner() { if( p ) { p->poison(); } }
 			};
 
-			void poison() { lk= nullptr; }
 			void
 			validate() const
 			{
-				assert( lk && "This Unlockable object, which is either suitable for r-value only, or experienced a nesting error -- it was poisoned" );
 				assert( lk->owns_lock() && "This Unlockable object was unlocked by someone, it cannot be used at this time." );
+				assert( this->alive() && "This Unlockable object, which is either suitable for r-value only, or experienced a nesting error -- it was poisoned" );
 			}
 
 			static Poisoner
@@ -48,72 +70,69 @@ namespace InfiniteMonkeys
 
 
         public:
-            Unlockable( ULock &i_lk ) : lk( &i_lk ) { assert( lk->owns_lock() ); }
-            Unlockable( ULock &&i_lk, Poisoner &&p= makePoison() ) : lk( &i_lk ) { assert( lk->owns_lock() ); p.p= this; }
+            Unlockable( ULock &i_lk ) : Poisonable( nullptr ), lk( &i_lk ) { assert( lk->owns_lock() ); }
+			// TODO: I think the Poisoner can use the Poisonable base class as its basis, but for now it's fine.
+			// In fact, I think we can detect on the line, rather than wait for the dtor, if the `Poisoner` is a child `Poisonable`.
+            Unlockable( ULock &&i_lk, Poisoner &&p= makePoison() ) : Poisonable( nullptr ), lk( &i_lk ) { assert( this->lk->owns_lock() ); p.p= this; }
 
 			Unlockable( OwningUnlockable &&, Poisoner &&p= makePoison() );
 			Unlockable( OwningUnlockable & );
 
             Unlockable( const Unlockable &copy )
-				: lk( copy.lk )
+				: Poisonable( nullptr /* maybe it should be: `&copy` */ ), lk( copy.lk )
             {
 				copy.validate();
             }
 
-			Unlockable()= default;
-
 			~Unlockable()
 			{
 				validate();
-				if( poisoner ) p->p= nullptr;
+				if( poisoner ) poisoner->p= nullptr;
 			}
 
 			[[nodiscard]] auto
 			promiscuous() &
 			{
+				// This one doesn't work, I think.
 				validate();
 
-				struct Guardian : boost::noncopyable
+				struct Promiscuous : Poisonable
 				{
 					private:
 						ULock *lk;
-						Unlockable *ul;
 
-						void poison() { ul= nullptr; }
 						void
 						validate()
 						{
-							assert( ul && "This Promiscuous object, which is suitable only for use when not chaste, was poisoned" );
-							assert( !lk->owns_lock() && "This Promiscuous object was locked by someone, it cannot be used at this time." );
+							assert( this->alive() && "This Promiscuous object, which is suitable only for use when not chaste, was poisoned" );
+							assert( !this->lk->owns_lock() && "This Promiscuous object was locked by someone, it cannot be used at this time." );
 						}
 
 					public:
-						Guardian( Unlockable *i_ul )
-							: lk( i_ul->lk ), ul( i_ul )
+						explicit Promiscuous( Unlockable *const i_ul )
+							: Poisonable( i_ul ), lk( i_ul->lk )
 						{
-							ul->poison();
 							lk->unlock();
 						}
 
-						~Guardian()
+						~Promiscuous()
 						{
-							validate();
-							lk->lock();
-							ul->lk= lk;
+							this->validate();
+							this->lk->lock();
 						}
 
+#if 0
 						[[nodiscard]] auto
 						chaste()
 						{
-							validate();
-							lk->lock();
-							Unlockable rv= *lk;
-							poison();
-							return rv;
+							this->validate();
+							this->lk->lock();
+							// TODO: Need to create ownership here.
 						}
+#endif
 				};
 
-				return Guardian( this );
+				return Promiscuous( this );
 			}
 
 			operator ULock &() & { validate(); return *lk; }
@@ -125,72 +144,72 @@ namespace InfiniteMonkeys
 		return Unlockable( lk );
 	}
 
-	class OwningUnlockable : boost::noncopyable
+	class OwningUnlockable : Poisonable
 	{
 		private:
 			ULock lk;
 
 			friend Unlockable;
 
+			explicit OwningUnlockable( Poisonable *const p, Mtx &mtx ) : Poisonable( p ), lk( mtx ) { assert( this->lk.owns_lock() ); }
+
 		public:
-			OwningUnlockable( Mtx &mtx ) : lk( mtx ) { assert( lk.owns_lock() ); }
+			OwningUnlockable( Mtx &mtx ) : Poisonable( nullptr ), lk( mtx ) { assert( this->lk.owns_lock() ); }
 
 			[[nodiscard]] auto
 			promiscuous() &
 			{
-				struct Guardian : boost::noncopyable
+				this->validate();
+
+				struct Promiscuous : Poisonable
 				{
 					private:
 						ULock *lk;
 
-						void poison() { lk= nullptr; }
 						void
 						validate()
 						{
-							assert( lk && "This Promiscuous object, which is suitable only for use when not chaste, was poisoned" );
 							assert( !lk->owns_lock() && "This Promiscuous object was locked by someone, it cannot be used at this time." );
+							assert( this->alive() && "This Promiscuous object, which is suitable only for use when not chaste, was poisoned" );
 						}
 
 					public:
-						Guardian( OwningUnlockable *i_ul )
-							: lk( &i_ul->lk ) 
+						Promiscuous( OwningUnlockable *const i_ul )
+							: Poisonable( i_ul ), lk( &i_ul->lk ) 
 						{
-							lk->unlock();
+							this->lk->unlock();
 						}
 
-						~Guardian()
+						~Promiscuous()
 						{
-							validate();
-							lk->lock();
+							this->validate();
+							this->lk->lock();
 						}
 
 						[[nodiscard]] auto
 						chaste()
 						{
-							validate();
-							lk->lock();
-							Unlockable rv{ *lk };
-							poison();
-							return rv;
+							this->validate();
+							return OwningUnlockable( this, *this->lk->mutex() );
 						}
 				};
 
-				return Guardian( this );
+				return Promiscuous( this );
 			}
 	};
 
 	Unlockable::Unlockable( OwningUnlockable &o )
-		: lk( &o.lk )
+		: Poisonable( nullptr ), lk( &o.lk )
 	{
-		assert( lk->owns_lock() );
+		assert( this->lk->owns_lock() );
 	}
 
 	Unlockable::Unlockable( OwningUnlockable &&o, Unlockable::Poisoner &&p )
-		: lk( &o.lk )
+		: Poisonable( nullptr ), lk( &o.lk ), poisoner( &p )
 	{
-		std::cerr << "Dangerous, but poisonable promote, via rvalue catching." << std::endl;
-		assert( lk->owns_lock() );
-		p.p= this;
+		std::cerr << "It's dangerous to go alone, so take this poisoner.  This allows promote, via rvalue case catching." << std::endl;
+		assert( this->lk->owns_lock() );
+		this->poisoner->p= this;
 	}
 
 	auto
@@ -202,8 +221,8 @@ namespace InfiniteMonkeys
 
 namespace std
 {
-	template<> InfiniteMonkeys::OwningUnlockable &&move( InfiniteMonkeys::OwningUnlockable & )= delete;
-	template<> InfiniteMonkeys::Unlockable &&move( InfiniteMonkeys::Unlockable & )= delete;
+	template<> InfiniteMonkeys::OwningUnlockable &&move( InfiniteMonkeys::OwningUnlockable & ) noexcept= delete;
+	template<> InfiniteMonkeys::Unlockable &&move( InfiniteMonkeys::Unlockable & ) noexcept= delete;
 }
 
 void disallowed();
@@ -215,7 +234,7 @@ f1( Unlockable u )
 	std::cerr << "F1 called" << std::endl;
 	auto prom= u.promiscuous();
 	auto chaste= prom.chaste();
-	auto p2= chaste.promiscuous();
+	//auto p2= chaste.promiscuous();
 
 	std::cerr << "Got all" << std::endl;
 
